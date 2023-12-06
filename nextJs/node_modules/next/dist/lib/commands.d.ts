60597fcee883aed4ad1e0f212ccf1379e614848f
Niketha Nethaji <131146523+nnethaji@users.noreply.github.com>
2023-12-06 14:05:47 +0530

    Delete nextJs/node_modules directory

--- nextJs/node_modules/next/dist/lib/commands.d.ts
+++
@@ -1,5 +0,0 @@
-import { getValidatedArgs } from './get-validated-args';
-export type CliCommand = (args: ReturnType<typeof getValidatedArgs>) => void;
-export declare const commands: {
-    [command: string]: () => Promise<CliCommand>;
-};
